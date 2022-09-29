28ed327ca222da9bea3525d9549a94eb8c96c2e7
KangHongJun <rkdghdwns97@naver.com>
2022-09-29 09:17:07 +0900

    220928 논문 리서치&정리

---
+++ craw_schedule.py
@@ -0,0 +1,28 @@
+from selenium import webdriver
+from selenium.webdriver import DesiredCapabilities
+
+# chrome_options = webdriver.ChromeOptions()
+# # chrome_options.add_argument('--headless')
+# chrome_options.add_argument("disable-gpu")
+# chrome_options.add_argument('--de')
+# chrome_options.add_argument('--no-sandbox')
+# chrome_options.add_argument('--disable-dev-shm-usage')
+# caps = DesiredCapabilities().CHROME
+# caps["pageLoadStrategy"] = "none"
+# driver = webdriver.Chrome('chromedriver', options=chrome_options)
+
+# 사이트 접속
+#driver.get('https://member.onstove.com/auth/login?inflow_path=lost_ark&game_no'
+ #          '=45&redirect_url=https%3a%2f%2flostark.game.onstove.com%2fMarket')
+#driver.maximize_window()
+
+#schedule_time = driver.find_elements_by_css_selector('#tbodyItemList > tr > td:nth-child(1) > div > span.name')
+
+keep = [1,2,3,4]
+while len(keep):
+    print("qwe")
+    qwe = 10<0.6
+    keep = keep[qwe]
+
+
+print(keep)
